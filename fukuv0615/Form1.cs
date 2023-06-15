609046b2d45f0897a18d5871d41a25d61524163c
岸野 <130636405+mugitya00@users.noreply.github.com>
2023-06-15 14:00:41 +0900

    復習問題

--- fukuv0615/Form1.cs
+++ fukuv0615/Form1.cs
@@ -1,10 +1,40 @@
 namespace fukuv0615
 {
+
     public partial class Form1 : Form
     {
+        int ans = rand.Next(0, 101);
+        int count = 0;
+        static Random rand = new Random();
+        int guess;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            MessageBox.Show($"{count}âÒñ⁄ÇÃíßêÌ");
+
+            guess = $"{textBox1}";
+            if (ans == guess)
+            {
+                MessageBox.Show("Bingo!!");
+            }
+            else if (ans < guess)
+            {
+                MessageBox.Show("Less");
+            }
+            else if (ans > guess)
+            {
+                MessageBox.Show("Greater");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
